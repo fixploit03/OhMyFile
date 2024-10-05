9c50c9a8ea81ae9f45f716f07175e34ba7000ac0
fixploit03 <164528883+fixploit03@users.noreply.github.com>
2024-10-05 19:23:17 +0700

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,31 +1,27 @@
 import hashlib
 
-def calculate_all_hashes(file_path):
-    # Daftar algoritma hash yang ingin dihitung
-    hash_algorithms = ['md5', 'sha1', 'sha256', 'sha512']
+# Daftar algoritma hash yang ingin dihitung
+hash_algorithms = ['md5', 'sha1', 'sha224', 'sha256', 'sha384', 'sha512', 'blake2b', 'blake2s']
 
-    # Membuat objek hash untuk setiap algoritma
-    hash_funcs = {algo: hashlib.new(algo) for algo in hash_algorithms}
+# Membuat objek hash untuk setiap algoritma
+hash_funcs = {algo: hashlib.new(algo) for algo in hash_algorithms}
 
-    try:
-        with open(file_path, 'rb') as file:
-            # Membaca file dalam blok
-            while chunk := file.read(8192):
-                for func in hash_funcs.values():
-                    func.update(chunk)
+file_path = input("[#] Masukkan path file: ")
 
-        # Menampilkan hasil hash dalam format heksadesimal untuk setiap algoritma
-        for algo, func in hash_funcs.items():
-            print(f"[+] {algo.upper()} hash dari file {file_path}: {func.hexdigest()}")
+try:
+    with open(file_path, 'rb') as file:
+        # Membaca file dalam blok
+        while chunk := file.read(8192):
+            for func in hash_funcs.values():
+                func.update(chunk)
 
-    except FileNotFoundError:
-        print(f"[!] File {file_path} tidak ditemukan.")
-    except PermissionError:
-        print(f"[!] Tidak memiliki izin untuk membuka file {file_path}.")
-    except Exception as e:
-        print(f"[!] Terjadi kesalahan: {e}")
+    # Menampilkan hasil hash dalam format heksadesimal untuk setiap algoritma
+    for algo, func in hash_funcs.items():
+        print(f"[+] {algo.upper()} hash dari file {file_path}: {func.hexdigest()}")
 
-if __name__ == "__main__":
-    file_path = input("[#] Masukkan path file: ")
-    calculate_all_hashes(file_path)
-  
+except FileNotFoundError:
+    print(f"[!] File {file_path} tidak ditemukan.")
+except PermissionError:
+    print(f"[!] Tidak memiliki izin untuk membuka file {file_path}.")
+except Exception as e:
+    print(f"[!] Terjadi kesalahan: {e}")
